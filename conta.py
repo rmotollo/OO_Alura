af3bc040438920409d1961b2eb14065ce98082f9
Rodrigo.Motollo <rodrigo.motollo@gmail.com>
2018-10-02 11:27:28 -0300

    OO_Project

--- conta.py
+++ conta.py
@@ -4,18 +4,37 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construção de objeto em {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("O saldo da conta é R${}".format(self.saldo))
+        print("O saldo da conta é R${}".format(self.__saldo))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
 
